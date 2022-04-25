2183370472e7674561d8c9e81c45832bcaeb3510
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-04-25 10:26:00 -0300

    Terminando a lógica do cronômetro

--- src/App.js
+++ src/App.js
@@ -7,16 +7,43 @@ class App extends Component {
     super(props);
     this.state = {
       numero: 0,
+      botao: "VAI",
     };
-
+    this.timer = null;
     this.vai = this.vai.bind(this);
     this.limpar = this.limpar.bind(this);
   }
   vai() {
-    alert("ola");
+    let state = this.state;
+
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = "VAI";
+      this.setState(state);
+    } else {
+      this.timer = setInterval(() => {
+        state.numero += 0.1;
+
+        this.setState(state);
+      }, 100);
+      state.botao = "PARAR";
+    }
+    this.setState(state);
   }
   limpar() {
-    alert("ola");
+    let state = this.state;
+
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+
+      this.setState(state);
+    }
+
+    state.numero = 0;
+    state.botao = "VAI";
+    this.setState(state);
   }
 
   render() {
@@ -26,9 +53,11 @@ class App extends Component {
         <a className="timer">{this.state.numero.toFixed(1)}</a>
         <div className="areaBtn">
           <a onClick={this.vai} className="botao">
-            VAI
+            {this.state.botao}
           </a>
-          <a  onClick={this.limpar}className="botao">LIMPAR</a>
+          <a onClick={this.limpar} className="botao">
+            LIMPAR
+          </a>
         </div>
       </div>
     );
